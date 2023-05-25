f88999fdfc11a1f18687a1cfd218531681e836c7
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-05-25 21:23:04 +0900

    画像を解析する処理を実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,22 @@
+import vision from "@google-cloud/vision";
 import * as functions from "firebase-functions";
 
-export const annotateImage = functions.https.onCall((data, context) => {
+const client = new vision.ImageAnnotatorClient();
+
+export const annotateImage = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
         throw new functions.https.HttpsError(
             "unauthenticated",
             "annotateImageは認証時に呼び出す必要があります。"
+        );
+    }
+    try {
+        return await client.annotateImage(data);
+    } catch (error: any) {
+        throw new functions.https.HttpsError(
+            "internal",
+            error.message,
+            error.details
         );
     }
 });
