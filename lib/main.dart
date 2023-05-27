da3a28a817cb8b6e7cf95b06b7643de9912e898f
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-05-27 19:01:42 +0900

    firebaseの初期化と各プラグインの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:for_ocr_practice/firebase_options.dart';
 import 'package:for_ocr_practice/receipt_loading_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
